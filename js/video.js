f4ce99c50d619aab4cfa8c39d5c1c6d1ea00811a
dantemitchell <145046281+dantemitchell@users.noreply.github.com>
2024-04-08 08:59:51 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -18,7 +18,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const volumeSpan = document.getElementById('volume');
 	const slowerButton = document.getElementById("slower");
 	const fasterButton = document.getElementById("faster");
+	const muteButton = document.getElementById('mute');
 	var skipButton = document.getElementById("skip");
+	const originalButton = document.getElementById('orig');
 	let slowDownCount = 0;
     
     playButton.addEventListener('click', function() {
@@ -55,6 +57,23 @@ document.addEventListener('DOMContentLoaded', function() {
         
         console.log("Current location of the video: " + video.currentTime);
     });
+	muteButton.addEventListener('click', function() {
+        video.muted = !video.muted;
+        if (video.muted) {
+            muteButton.textContent = 'Unmute';
+        } else {
+            muteButton.textContent = 'Mute';
+        }
+    });
+
+	var vintageButton = document.getElementById("vintage");
+    vintageButton.addEventListener("click", function() {
+        video.classList.toggle("oldSchool");
+    });
+	originalButton.addEventListener('click', function() {
+		const video = document.querySelector('.video');
+		video.classList.remove('oldSchool');
+	});
 
     const slider = document.getElementById('slider');
     slider.addEventListener('input', function() {
